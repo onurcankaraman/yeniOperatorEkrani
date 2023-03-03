904d47f5ce77a90c1dd1f23e842e6dd657c50e10
ONUR CAN KARAMAN <onurcankaraman340@gmail.com>
2023-03-04 00:19:49 +0300

    Güncelleme

--- src/views/Application.vue
+++ src/views/Application.vue
@@ -1,3 +1,16 @@
+
 <template>
-    Merhaba
+    <div class="container py-5 h-10">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-12 col-md-8 col-lg-6 col-xl-5 border">
+      Uygulama Alanı <br>
+      Şu Link İyi https://stackblitz.com/edit/vue-1bngkw?file=src%2FApp.vue
+      
+    </div>
+    <div class="col-12 col-md-8 col-lg-6 col-xl-5 border">
+      Bu Butonu Çoğaltacağız <br>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+  </div>
+</div>
 </template>
